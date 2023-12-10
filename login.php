1f0907b0cb3c88ed0c4f7513aa3fef3bf8c2b54a
Rakshanda Giri <rakshandagiri123@gmail.com>
2023-12-10 15:13:47 +0530

    login n signup frontend created

---
+++ login.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>
+            Login
+        </title>
+        <link rel="stylesheet" href="assets/css/login.css">
+    </head>
+    <body>
+            <form class="form">
+            <p class="title">Login </p>
+            <p class="message">Login now and get full access to our website. </p>
+                    
+            <label>
+                <input required="" placeholder="" type="email" class="input">
+                <span>Email</span>
+            </label> 
+                
+            <label>
+                <input required="" placeholder="" type="password" class="input">
+                <span>Password</span>
+            </label>
+            <button class="submit">Login</button>
+            <p class="signin">Already have an acount ? <a href="signup.php">Signup</a> </p>
+        </form>
+    </body>
+</html>
---
+++ signup.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>
+            Signup
+        </title>
+        <link rel="stylesheet" href="assets/css/login.css">
+    </head>
+    <body>
+            <form class="form">
+            <p class="title">Signup </p>
+            <p class="message">Signup now and get full access to our website. </p>
+            <div class="flex">
+                <label>
+                    <input required="" placeholder="" type="text" class="input">
+                    <span>Firstname</span>
+                </label>
+
+                <label>
+                    <input required="" placeholder="" type="text" class="input">
+                    <span>Lastname</span>
+                </label>
+            </div>  
+                    
+            <label>
+                <input required="" placeholder="" type="email" class="input">
+                <span>Email</span>
+            </label> 
+                
+            <label>
+                <input required="" placeholder="" type="password" class="input">
+                <span>Password</span>
+            </label>
+            <label>
+                <input required="" placeholder="" type="password" class="input">
+                <span>Confirm password</span>
+            </label>
+            <button class="submit">Signup</button>
+            <p class="signin">Already have an acount ? <a href="login.php">Login</a> </p>
+        </form>
+    </body>
+</html>
